865af7286334485a797189ef14f86fd6b09d19a3
Kirill Gugaev <kvgugaev@avito.ru>
2017-06-25 23:55:31 +0300

    Add router

--- handlers.go
+++ handlers.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/julienschmidt/httprouter"
 )
 
-func mainpage(w http.ResponseWriter, r *http.Request) {
+func mainpage(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
